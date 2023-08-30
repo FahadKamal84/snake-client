5eec27514acde9ab764a8a37ed9debe259633a07
Fahad Kamal <fahadkamal@hotmail.com>
2023-08-30 15:18:28 -0400

    setup stdin by creating two functions setupInput and handleUserInput so that players can send keyboard commands

--- play.js
+++ play.js
@@ -3,4 +3,25 @@ const connect = require('./client');
 
 console.log("Connecting ...");
 
+
+// setup interface to handle user input from stdin
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput)
+  return stdin;
+};
+
 connect();   // establishes a connection with the game server
+
+setupInput();
+// \u0003 maps to ctrl+c input
